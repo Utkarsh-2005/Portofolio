83194cc1519902fc3165a2d51f271608ea4ed95c
Utkarsh <utkarshjha.4009@gmail.com>
2025-03-15 20:12:23 +0530

    Updated  portfolio site

--- app/page.js
+++ app/page.js
@@ -91,7 +91,8 @@ export default function Home() {
       )}
     <div className="overflow-clip flex-column" ref={vantaRef}>
       <div className={`z-[2] backdrop-blur-md flex flex-row top-0 text-white p-5 bg-black bg-opacity-10 bg-blur-50 sticky navbar ${illumminate ? 'illumminate' : ''} ${isScrolled ? 'scrolled' : ''}`}>
-        <a className="mr-auto bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded" href="https://drive.google.com/file/d/1jVtPHsxZU8F_Ro4s2Ih18C8Lgel6qca5/view?usp=sharing">Resume</a>
+        <a className="mr-auto bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded" href="https://drive.google.com/file/d/1WUdeuCX-4wbDKQURBhT31-mwfrcNjy5t/view?usp=sharing" target="_blank" 
+  rel="noopener noreferrer">Resume</a>
         <Link href="#about">
         <p className="ml-auto md:mr-3 p-1 hidden sm:block">About me</p>
         </Link>
@@ -113,9 +114,9 @@ export default function Home() {
             <Card img="/profile.jpg" data="I am Second Year B.Tech Student in KIIT University. I am passionate about Web Development." title="About me"/>
             </div>
             <div id="skills" className="space-y-20">
-            <Card title="Frontend Skills" data="React.js, Next.js, Tailwind, Bootsrap, Material Ui"/>
-            <Card title="Backend Skills" data="ExpressJS, NodeJS, Flask"/>
-            <Card title="Databases" data="MongoDB, Firebase"/>
+            <Card title="Frontend Skills" data="React.js, Next.js, Typescript, Tailwind, Bootsrap, Material Ui"/>
+            <Card title="Backend Skills" data="ExpressJS, NodeJS, Flask, NextJS API Routes"/>
+            <Card title="Databases" data="MongoDB, PostgreSQL, Firebase, AstraDB"/>
             </div>
             <div className="w-screen sm:max-w-screen-md sm:p-[5vh] overflow-hidden sm:mt-20 m-10">
             <h1 className="text-white text-4xl sm:mb-[-20vh] mb-[-15vh] text-center mt-20 sm:mt-0">Projects</h1>
@@ -125,14 +126,20 @@ export default function Home() {
       navigation={true}
       pagination={{ clickable: true }}
       autoplay={{ delay: 3000 }}>
+       <SwiperSlide>
+      <ShowCase link="https://linkedboost.vercel.app/" repo="https://github.com/Utkarsh-2005/LinkedBoost" img="/linkedboost.png" data="A platform to evaluate your LinkedIn profile and posts from your URL with AI-driven insights. Built with Next.js, TypeScript, Tailwind CSS, Prisma/MongoDB, Astra DB, and NextAuth. Uses Gemini-powered RAG to identify improvement areas."/>
+      </SwiperSlide>
+      <SwiperSlide>
+      <ShowCase link="https://briefbuzz.vercel.app/" repo="https://github.com/Utkarsh-2005/BriefBuzz" img="/briefbuzz.png" data="A daily news web-app with over 50 active users leveraging the power of Gemini to deliver concise reports. Completely built on NextJS including the backend."/>
+      </SwiperSlide>
+      <SwiperSlide>
+      <ShowCase link="https://chai2go.vercel.app/" repo="https://github.com/Utkarsh-2005/Chai2Go_Frontend" img="/chai2go.png" data="An ordering system with a robust admin panel created using ViteJS, TypeScript, TailwindCSS and Socket. Secured authentication with JWT."/>
+      </SwiperSlide>
       <SwiperSlide>
       <ShowCase link="https://portofolio-sigma-liart.vercel.app/" repo="https://github.com/Utkarsh-2005/Portofolio" img="/portofolio.png" data="This is my portfolio site created using NextJS, TailwindCSS and TypeScript. It is fully responsive and implements external libraries like Three.JS, Vanta.JS and Swiper.JS. "/>
       </SwiperSlide>
       <SwiperSlide>
       <ShowCase link="https://utkarshflix.netlify.app/" repo="https://github.com/Utkarsh-2005/Utkarsh-flix" img="/utkarshflix.png" data="This is a clone of the Netflix landing page created with ReactJS and an API from RapidAPI and implements concepts like filtering and API calls."/>
-      </SwiperSlide>
-      <SwiperSlide>
-      <ShowCase link="https://book-store-frontend-kappa.vercel.app/" repo="https://github.com/Utkarsh-2005/BookStore_Frontend" img="/bookstore.png" data="This the MERN stack web-app that I created with a follow along project. This app has CRUD operations available."/>
       </SwiperSlide>
     </Swiper>
     </div>
@@ -141,12 +148,9 @@ export default function Home() {
     </div>
     <div>
     <footer className="footer" id="contact">
-    <div className="waves">
+    {/* <div className="waves">
       <div className="wave" id="wave1"></div>
-      <div className="wave" id="wave2"></div>
-      <div className="wave" id="wave3"></div>
-      <div className="wave" id="wave4"></div>
-    </div>
+    </div> */}
     <ul className="social-icon">
       <li className="social-icon__item"><a className="social-icon__link" href="https://github.com/Utkarsh-2005">
           <img src="/github.png" className="h-[28px] mb-[2px]"></img>
